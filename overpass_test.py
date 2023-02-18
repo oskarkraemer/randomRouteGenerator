2fd1defc71b16ba87eb7d65d36858f4b6bbe026f
oskarkraemer <oskarkraemer@gmail.com>
2023-02-18 16:27:20 +0100

    added overpassAPI test script
    (is pretty slow compared to first approach)

---
+++ overpass_test.py
@@ -0,0 +1,24 @@
+import overpy
+
+api = overpy.Overpass()
+
+forbidden_highways = ["motorway", "trunk", "steps", "motorway_link", "trunk_link"]
+bounding_box = (50.7, 7.1, 50.8, 7.25)
+
+query = 'way["highway"]'
+for highway in forbidden_highways:
+    query += '["highway"!="{}"]'.format(highway)
+
+query += '{};out;'.format(bounding_box)
+
+print(query)
+
+result = api.query(query)
+
+#result = api.query("""way["highway"]["highway"!="motorway"]["highway"!="trunk"]["highway"!="steps"]["highway"!="motorway_link"]["highway"!="trunk_link"](50.7,7.1,50.8,7.25);out;""")
+
+for way in result.ways:
+    print("Highway: %s" % way.tags.get("highway", "n/a"))
+    nodes = way.get_nodes(resolve_missing=True)
+    for node in nodes:
+        print("Node %s: lat=%f, lon=%f" % (node.id, node.lat, node.lon))
